4e5e43396866a706f47fb1a9d38f77c15e5027cd
yusufisbilir <dev.yusufisbilir@gmail.com>
2025-03-12 23:19:52 +0300

    confetti, sound

--- src/app/(timers)/layout.tsx
+++ src/app/(timers)/layout.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import ExamSelect from '@/components/ExamSelect'
+import FinishedTimerConfetti from '@/components/FinishedTimerConfetti'
 import FinishedTimerOverlay from '@/components/FinishedTimerOverlay'
 import TimerActions from '@/components/TimerActions'
 import { useTimer } from '@/contexts/TimerContext'
@@ -13,11 +14,14 @@ export default function RootLayout({
   const { state } = useTimer()
 
   return (
-    <section className="timers_layout_container">
-      {state.isFinished && <FinishedTimerOverlay />}
-      <ExamSelect />
-      {children}
-      <TimerActions />
-    </section>
+    <>
+      {state.isFinished && <FinishedTimerConfetti />}
+      <section className="timers_layout_container z-40">
+        {state.isFinished && <FinishedTimerOverlay />}
+        <ExamSelect />
+        {children}
+        <TimerActions />
+      </section>
+    </>
   )
 }
