40fc3720597ae92082163798d00ed5d625461421
yusufisbilir <dev.yusufisbilir@gmail.com>
2025-03-13 14:53:53 +0300

    remove unused imports

--- src/components/examPractice/ExamSelect.tsx
+++ src/components/examPractice/ExamSelect.tsx
@@ -3,15 +3,9 @@
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '../ui/select'
 import { EXAM } from '@/constants/constants'
 import { useTimer } from '@/contexts/TimerContext'
-import { Input } from '@/components/ui/input'
-import { useEffect } from 'react'
 
 const ExamSelect = () => {
   const { state, dispatch } = useTimer()
-
-  // useEffect(() => {
-  //   if (state.selectedExam === 'Custom') dispatch({ type: 'SET_CUSTOM_MINUTES', payload: '60' })
-  // }, [state.selectedExam])
 
   return (
     <div className="flex items-center gap-x-4">
@@ -32,22 +26,8 @@ const ExamSelect = () => {
           <SelectItem value="AYT" className="text-orange-950">
             {`AYT (${EXAM.AYT.duration} dakika)`}
           </SelectItem>
-          {/* <SelectItem value="Custom" className="text-orange-950">
-            Özel Süre
-          </SelectItem> */}
         </SelectContent>
       </Select>
-
-      {/* {state.selectedExam === 'Custom' && (
-        <Input
-          type="number"
-          value={state.customMinutes}
-          onChange={(e) => dispatch({ type: 'SET_CUSTOM_MINUTES', payload: e.target.value })}
-          placeholder="Dakika girin"
-          min={0}
-          className="w-full p-2 rounded-md bg-white border border-orange-300 text-orange-950 placeholder:text-orange-500"
-        />
-      )} */}
     </div>
   )
 }
