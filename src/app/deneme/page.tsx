acd22dea0e3228296ce8c8dcb9a4a0cd8578a97f
yusufisbilir <dev.yusufisbilir@gmail.com>
2025-03-13 21:57:30 +0300

    info boxes

--- src/app/deneme/page.tsx
+++ src/app/deneme/page.tsx
@@ -4,21 +4,46 @@ import ExamSelect from '@/components/examPractice/ExamSelect'
 import FinishedTimerConfetti from '@/components/examPractice/FinishedTimerConfetti'
 import FinishedTimerOverlay from '@/components/examPractice/FinishedTimerOverlay'
 import TimerActions from '@/components/examPractice/TimerActions'
+import { Button } from '@/components/ui/button'
 import { useTimer } from '@/contexts/TimerContext'
+import { useLocalStorage } from 'usehooks-ts'
 
 const Page = () => {
   const { state } = useTimer()
+  const [infoBoxVisibilities, setInfoBoxVisibilities] = useLocalStorage('infoBoxVisibilities', {
+    exampPractice: true,
+  })
 
   return (
-    <>
+    <section className="flex flex-col gap-y-2 my-4">
       {state.isFinished && <FinishedTimerConfetti />}
-      <section className="centered_card_container z-40">
+      {infoBoxVisibilities.exampPractice && (
+        <div className="centered_card_container gap-y-2">
+          <h1 className="font-semibold">İşte o saat 😱</h1>
+          <p>
+            Gerçek sınavda bu saate bakarak kalan süreni hesaplayacaksın.
+            <br />
+            Havalı kronometreler veya pomodoro uygulamaları yok.
+            <br />
+            Bitince bildirim sesi duyabilir ve biraz konfeti görebilirsin.
+            <br />
+            Başarılar dilerim ❤️
+          </p>
+          <Button
+            className="max-w-fit self-end"
+            onClick={() => setInfoBoxVisibilities((prev) => ({ ...prev, exampPractice: false }))}
+          >
+            Anladım Hocam, Hallederiz
+          </Button>
+        </div>
+      )}
+      <div className="centered_card_container z-40">
         {state.isFinished && <FinishedTimerOverlay />}
         <ExamSelect />
         <Clock />
         <TimerActions />
-      </section>
-    </>
+      </div>
+    </section>
   )
 }
 
--- src/app/gunluk_deneme/page.tsx
+++ src/app/gunluk_deneme/page.tsx
@@ -1,5 +1,49 @@
-const page = () => {
-  return <div>dailyexampractice</div>
+'use client'
+import Clock from '@/components/examPractice/Clock'
+import ExamSelect from '@/components/examPractice/ExamSelect'
+import FinishedTimerConfetti from '@/components/examPractice/FinishedTimerConfetti'
+import FinishedTimerOverlay from '@/components/examPractice/FinishedTimerOverlay'
+import TimerActions from '@/components/examPractice/TimerActions'
+import { Button } from '@/components/ui/button'
+import { useTimer } from '@/contexts/TimerContext'
+import { useLocalStorage } from 'usehooks-ts'
+
+const Page = () => {
+  const { state } = useTimer()
+  const [infoBoxVisibilities, setInfoBoxVisibilities] = useLocalStorage('infoBoxVisibilities', {
+    dailyExamPractice: true,
+  })
+
+  return (
+    <section className="flex flex-col gap-y-2 my-4">
+      {state.isFinished && <FinishedTimerConfetti />}
+      {infoBoxVisibilities.dailyExamPractice && (
+        <div className="centered_card_container gap-y-2">
+          <h1 className="font-semibold">Gerçek Sınav Deneyimi</h1>
+          <p>
+            Her sabah 10:15'te başlar. <br />
+            Gerçek sınavda bu saate bakarak kalan süreni hesaplayacaksın. Havalı kronometreler veya
+            pomodoro uygulamaları yok. <br />
+            Başarılar dilerim ❤️
+          </p>
+          <Button
+            className="max-w-fit self-end"
+            onClick={() =>
+              setInfoBoxVisibilities((prev) => ({ ...prev, dailyExamPractice: false }))
+            }
+          >
+            Anladım Hocam, Hallederiz
+          </Button>
+        </div>
+      )}
+      <div className="centered_card_container z-40">
+        {state.isFinished && <FinishedTimerOverlay />}
+        <ExamSelect />
+        <Clock />
+        <TimerActions />
+      </div>
+    </section>
+  )
 }
 
-export default page
+export default Page
