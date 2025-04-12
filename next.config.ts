6ecc9d0828da181493e6c59a75456ea598bd95e2
yusufisbilir <dev.yusufisbilir@gmail.com>
2025-04-12 11:06:46 +0300

    fix next issues

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['img.clerk.com'],
+    remotePatterns: [{ hostname: 'img.clerk.com' }],
   },
 }
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import '../globals.css'
+import './globals.css'
 import Sidebar from '@/components/layout/Sidebar'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
