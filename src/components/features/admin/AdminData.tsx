f8be6631e10ba20376c80209aeba15ad311e0c21
yusufisbilir <dev.yusufisbilir@gmail.com>
2025-04-16 09:33:12 +0300

    edit admin panel

--- src/components/features/admin/AdminData.tsx
+++ src/components/features/admin/AdminData.tsx
@@ -2,7 +2,7 @@ import { UserList } from './UserList'
 import { adminService } from '@/services/admin.service'
 
 export async function AdminData() {
-  const users = await adminService.getAllUsers()
+  const userProfiles = await adminService.getAllProfilesWithUniversityProgram()
   const examCounts = await adminService.getTotalExamAttemptsPerUser()
-  return <UserList users={users} examCounts={examCounts} />
+  return <UserList userProfiles={userProfiles ?? []} examCounts={examCounts} />
 }
